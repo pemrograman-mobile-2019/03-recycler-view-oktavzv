94faf4d06668653bcdd33cd2773090bc489eb29a
Oktavian Yudha <pratama.oktavian66@gmail.com>
2019-02-21 12:10:42 +0700

    RecycleView - Latihan

---
+++ app/src/main/java/com/oktavianyudha/recycleview/MainActivity.java
@@ -0,0 +1,34 @@
+package com.oktavianyudha.recycleview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.oktavianyudha.recycleview.adapter.ContactsAdapter;
+import com.oktavianyudha.recycleview.models.Contact;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Contact> contacts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // Initialize contacts
+        contacts = Contact.createContactsList(20);
+        // Create adapter passing in the sample user data
+        ContactsAdapter adapter = new ContactsAdapter(contacts);
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+        // That's all!
+    }
+}
